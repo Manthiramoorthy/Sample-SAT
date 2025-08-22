470c383033b41e4cb1ea4885665a19c809b00ac3
Manthiramoorthy <manthiramoorthyk15@gmail.com>
2025-08-22 19:50:13 +0530

    List

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello word</h1>
+      <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+      </ol>
     </div>
   );
 }
