3808a76f340a400dfa864ad65e6908bbedc86d41
Manthiramoorthy <manthiramoorthyk15@gmail.com>
2025-08-23 18:29:07 +0530

    Image add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello word</h1>
+      <img src={logo} className="App-logo" alt="logo" />
       <ol>
         <li>Item 1</li>
         <li>Item 2</li>
